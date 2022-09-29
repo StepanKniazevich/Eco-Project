a2cff347c849d59d93d16af5fe534a1b3a1f2ddf
stepankniazevich <steoan2000kniaz@outlook.com>
2022-09-29 10:28:49 +0300

    add fix slider

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
-let swiper = new Swiper(".ecoCards", {
-    autoplay: {
-        delay: 500,
-      },
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
     navigation: {
       nextEl: ".ecoCards__next",
       prevEl: ".ecoCards__prev",
