87b2e11ef1ee26868263d83810b8e36b2e52514d
stepankniazevich <steoan2000kniaz@outlook.com>
2022-09-29 10:00:41 +0300

    add eco-slider

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let swiper = new Swiper(".ecoCards", {
+    autoplay: {
+        delay: 500,
+      },
+    navigation: {
+      nextEl: ".ecoCards__next",
+      prevEl: ".ecoCards__prev",
+    },
+  });
