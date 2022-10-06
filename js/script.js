a337871bc6dea79957f21b4e58db087670e47a86
stepankniazevich <steoan2000kniaz@outlook.com>
2022-10-06 10:12:11 +0300

    add hamb-menu

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
-let swiper = new Swiper(".ecoCards", {
-    slidesPerView: 2,
-    spaceBetween: 50,
-    slidesPerGroup: 1,
+//-----------------ecoSlider-----------------------------
+let createSwiper = (slidesPerViewCount,spaceBetweenCout,slidesPerGroupCout) => {
+  let swiper = new Swiper(".ecoCards", {
+    slidesPerView: slidesPerViewCount,
+    spaceBetween: spaceBetweenCout,
+    slidesPerGroup: slidesPerGroupCout,
     loop: true,
     loopFillGroupWithBlank: true,
     navigation: {
@@ -13,3 +15,33 @@ let swiper = new Swiper(".ecoCards", {
         delay: 2000,
       }
   });
+}
+
+
+if ( document.documentElement.clientWidth <= 900) { 
+  createSwiper(1,2,1);
+ }
+else {
+  createSwiper(2,50,1);
+}
+
+//--------------hambMenu--------------------------
+
+const hamb = document.querySelector("#hamb");
+const popup = document.querySelector("#popup");
+const menu = document.querySelector(".menu__list").cloneNode(1);
+
+const hambHandler = (e) => {
+  e.preventDefault();
+  popup.classList.toggle("open");
+  renderPopup();
+}
+
+const renderPopup = () => {
+  popup.appendChild(menu);
+  console.log(popup);
+
+}
+hamb.addEventListener("click", hambHandler );
+
+
